7cc9478ae9174df102c3c6ee5f6f271b5d1ca91a
tomeryoel <Tomerys@gmail.com>
2024-04-27 15:58:21 +0300

    Initial commit

---
+++ src/test/java/tomash/project/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package tomash.project.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
