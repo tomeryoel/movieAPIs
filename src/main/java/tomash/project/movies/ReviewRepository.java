d0b0672524bfd21f210518f50ced113ac95c9b4a
tomeryoel <Tomerys@gmail.com>
2024-04-27 15:58:21 +0300

    Basic skeleton

---
+++ src/main/java/tomash/project/movies/ReviewRepository.java
@@ -0,0 +1,8 @@
+package tomash.project.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
