27cd3e4c7f77ec02cafb79b3d6aa1d422bac7ca2
tomeryoel <96529795+tomeryoel@users.noreply.github.com>
2024-05-09 16:23:07 +0300

    Update application.properties

--- src/main/java/tomash/project/movies/ReviewRepository.java
+++ src/main/java/tomash/project/movies/ReviewRepository.java
@@ -2,7 +2,9 @@ package tomash.project.movies;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 
 }
